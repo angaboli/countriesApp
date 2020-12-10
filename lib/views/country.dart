666c987167444db185b6237427e2239bb2c7f366
Ngabonziza Aimé <nzizaba@gmail.com>
2020-12-11 00:34:32 +0100

    countries details

--- lib/views/country.dart
+++ lib/views/country.dart
@@ -11,7 +11,63 @@ class Country extends StatelessWidget {
         backgroundColor: Colors.blue,
         title: Text(country['name']),
       ),
-      body: ,
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 1,
+          ),
+          children: <Widget>[
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.symmetric(vertical: 50, horizontal: 0),
+              child: Image(
+                image: NetworkImage(country['flag']),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              child: Center(
+                child: Table(
+                    children: [
+                      TableRow(children: [
+                        Text('Name', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(country['name'], style: TextStyle(fontSize: 20)),
+                      ]),
+                      TableRow(children: [
+                        Text('Capital', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(country['capital'], style: TextStyle(fontSize: 20))
+                      ]),
+                      TableRow(children: [
+                        Text('Continent', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(country['region'], style: TextStyle(fontSize: 20))
+                      ]),
+                      TableRow(children: [
+                        Text('Population', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(country['population'].toString(), style: TextStyle(fontSize: 20))
+                      ]),
+                      TableRow(children: [
+                        Text('Area', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(country['area'].toString(), style: TextStyle(fontSize: 20))
+                      ]),
+                      TableRow(children: [
+                        Text('TimeZone', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(country['timezones'].toString(), style: TextStyle(fontSize: 20))
+                      ]),
+                      TableRow(children: [
+                        Text('Currency', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(country['currencies'].toString(), style: TextStyle(fontSize: 20))
+                      ]),
+                    ]
+                ),
+              ),
+            )
+            ]
+        ),
+
+        ),
+
+
     );
   }
 }
